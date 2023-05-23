8160bf843f3474e7783daa366ff291531c2a84f9
Gedeon <infomng2022@gmail.com>
2023-05-24 00:30:29 +0200

    first commit on the secondBranch

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -51,7 +51,7 @@ const LeftNav = () => {
         })}
         <hr className="my-5 border-white/[0.2]" />
         <div className="text-white/[0.5] text-[12px]">
-          Clone by: JS Dev Hindi
+          Copyright © {new Date().getFullYear()} GD Media
         </div>
       </div>
     </div>
